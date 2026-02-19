3a50c32c44208303980f1f5bdcc4d28eb72eedc5
Vikingnope <aidsch0605@outlook.com>
2026-02-19 20:00:17 +0100

    chore: format nix file

--- flake.nix
+++ flake.nix
@@ -4,11 +4,18 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-      in {
+      in
+      {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             rustup
@@ -30,5 +37,6 @@
             rustup target add wasm32-unknown-unknown
           '';
         };
-      });
+      }
+    );
 }
