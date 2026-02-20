e8b78d5dce188b61bea690d538bccd61b8468fa5
Vikingnope <aidsch0605@outlook.com>
2026-02-20 09:53:03 +0100

    chore: update nix flake to be reproducable

--- flake.nix
+++ flake.nix
@@ -1,42 +1,84 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            rustup
-            pkg-config
-            dioxus-cli
+  outputs = {
+    self,
+    flake-parts,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+        "aarch64-linux"
+      ];
 
-            # Desktop (WebKit/GTK)
-            gtk3
-            webkitgtk_4_1
-            glib
-            libsoup_3
+      perSystem = {
+        pkgs,
+        lib,
+        system,
+        ...
+      }: let
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [
+            "rust-src"
+            "rust-analyzer"
+            "clippy"
+          ];
+          targets = ["wasm32-unknown-unknown"];
+        };
+
+        buildInputs =
+          (with pkgs; [
             openssl
-            cacert
-            xdotool
+            pkg-config
+          ])
+          ++ lib.optionals pkgs.stdenv.isLinux (
+            with pkgs; [
+              glib
+              gtk3
+              libsoup_3
+              webkitgtk_4_1
+              xdotool
+              libayatana-appindicator
+              librsvg
+            ]
+          )
+          ++ lib.optionals pkgs.stdenv.isDarwin (
+            with pkgs.darwin.apple_sdk.frameworks; [
+              SystemConfiguration
+              IOKit
+              Carbon
+              WebKit
+              Security
+              Cocoa
+            ]
+          );
+      in {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [inputs.rust-overlay.overlays.default];
+        };
+
+        devShells.default = pkgs.mkShell {
+          inherit buildInputs;
+          nativeBuildInputs = with pkgs; [
+            rustToolchain
+            dioxus-cli
+            lld
+            tailwindcss_4
+            watchman
           ];
 
           shellHook = ''
-            rustup default stable
-            rustup target add wasm32-unknown-unknown
+            export RUST_SRC_PATH="${rustToolchain}/lib/rustlib/src/rust/library"
           '';
         };
-      }
-    );
+      };
+    };
 }
